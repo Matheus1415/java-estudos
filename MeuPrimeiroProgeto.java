0de4f33d1b794cc12315ea5090ab1337998a733b
Matheus1415 <115950084+Matheus1415@users.noreply.github.com>
2022-10-19 20:15:28 -0300

    required

---
+++ MeuPrimeiroProgeto.java
@@ -0,0 +1,14 @@
+package com.mycompany.meuprimeiroprogeto;
+
+import java.util.Date;
+
+public class HoraDoSistema {
+    public static void main(String[] args) {
+    Date relogio = new Date ();
+        System.out.println("à hora do sistema é");
+        System.out.println (relogio.toString());
+    
+    
+    }//conteudo
+    
+}//fecha a class
